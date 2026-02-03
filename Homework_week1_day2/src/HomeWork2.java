959a8e69c11b9f64c6f4573c53a01e72af450f77
Alejandro Lopez <lopezalejandro5b@gmail.com>
2026-02-02 21:53:56 -0800

    [Homework #2] Write java code  to merge two sorted arrays + Junit test

---
+++ Homework_week1_day2/src/HomeWork2.java
@@ -0,0 +1,47 @@
+package Homework_week1_day2.src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ Alejandro Lopez - CS146 - SJSU
+
+ So my approach is similar to what was discussed at class, and I got to understand better
+ how to handle the remaining array in the case when one of then is shorter than the other.
+
+ I simply used two pointers, one for each array and add them to a result array as it goes.
+
+ Space complexity: O(n)
+ Time complexity: O(n)
+
+*/
+
+public class HomeWork2 {
+    static void main() {
+        System.out.print(mergeSort(
+                List.of(1,2,4,5),
+                List.of(3,4,5)
+        ));
+    }
+
+    public static List<Integer> mergeSort(List<Integer> left, List<Integer> right) {
+
+        int i = 0, j = 0;
+        List<Integer> result = new ArrayList<>();
+
+        while (i < left.size() && j < right.size()) {
+
+            if (left.get(i) <= right.get(j)) {
+                result.add(left.get(i));
+                i++;
+            } else {
+                result.add(right.get(j));
+                j++;
+            }
+        }
+
+        result.addAll(left.subList(i, left.size()));
+        result.addAll(right.subList(j, right.size()));
+        return result;
+    }
+}
---
+++ Homework_week1_day2/src/MainHomework2Test.java
@@ -0,0 +1,38 @@
+package Homework_week1_day2.src;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class MainHomework2Test {
+    @Test
+    void testMergeSort(){
+        List<Integer> result = (HomeWork2.mergeSort(List.of(1,2,4,5), List.of(3,4,5)));
+        Assertions.assertEquals(List.of(1,2,3,4,4,5,5), result);
+    }
+
+    @Test
+    void mergesWithOverlap() {
+        Assertions.assertEquals(List.of(1,2,3,4,4,5,5), HomeWork2.mergeSort(List.of(1,2,4,5), List.of(3,4,5)));
+    }
+
+    @Test
+    void mergesWithEmptyRight() {
+        Assertions.assertEquals(List.of(1,2,4,5), HomeWork2.mergeSort(List.of(1,2,4,5), List.of()));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
