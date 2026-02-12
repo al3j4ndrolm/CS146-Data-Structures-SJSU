0765cac6a05a1d185f0d7050d9272f1385abf739
Alejandro Lopez <lopezalejandro5b@gmail.com>
2026-02-12 15:45:28 -0800

    [+] Implement maxHeapify function

--- Homework_week3/HomeworkWeek3.java
+++ Homework_week3/HomeworkWeek3.java
@@ -4,4 +4,26 @@ public class HomeworkWeek3 {
     static void main() {
         /*TODO*/
     }
+
+    void maxHeapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            maxHeapify(arr, n, largest);
+        }
+    }
 }
