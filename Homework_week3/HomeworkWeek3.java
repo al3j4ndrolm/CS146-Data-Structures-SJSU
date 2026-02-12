fb012bb9c8111d1deadf710ff7dc94104d6b05bd
Alejandro Lopez <lopezalejandro5b@gmail.com>
2026-02-12 13:53:25 -0800

    [+] Implement WordFrequency class

--- Homework_week3/HomeworkWeek3.java
+++ Homework_week3/HomeworkWeek3.java
@@ -4,5 +4,4 @@ public class HomeworkWeek3 {
     static void main() {
         /*TODO*/
     }
-
 }
---
+++ Homework_week3/WordFrequency.java
@@ -0,0 +1,11 @@
+package Homework_week3;
+
+public class WordFrequency {
+    String word;
+    Integer frequency;
+
+    WordFrequency(String word, Integer frequency) {
+        this.word = word;
+        this.frequency = frequency;
+    }
+}
